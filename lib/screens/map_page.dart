f83244efa532bf375313a0d1a5406d706c31c6cf
WTPENGUIN <ccho2932@naver.com>
2024-01-30 14:48:47 +0900

    아이콘 추가, 맵에서 기체 마커로 사용

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:peachgs_flutter/model/multi_vehicle_manage.dart';
+import 'package:peachgs_flutter/widget/svg_image.dart';
 
 class MapWidget extends StatefulWidget {
   const MapWidget({Key? key}) : super(key: key);
@@ -13,19 +14,15 @@ class MapWidget extends StatefulWidget {
 }
 
 class _MapWidgetState extends State<MapWidget> {
-  // final TextEditingController _serverAddressController = TextEditingController();
-  // final TextEditingController _serverPortController = TextEditingController();
-  // final ValueNotifier<String> _selectedProtocol = ValueNotifier<String>('UDP');
-
   List<Marker> vehiclesPosition() {
     List<Marker> markers = [];
     for(var vehicle in MultiVehicle().allVehicles()) {
       markers.add(
         Marker(
           point: LatLng(vehicle.latitude, vehicle.longitude),
-          child: const Icon(
-            Icons.location_on,
-            color: Colors.red,
+          child: const SVGImage(
+            route: 'assets/VehicleIcon.svg',
+            size: Size(50,50)
           )
         )
       );
@@ -59,93 +56,4 @@ class _MapWidgetState extends State<MapWidget> {
       ]
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     home: Scaffold(
-  //       body: SafeArea(
-  //         child: Stack(
-  //           children: [
-  //             Consumer<MultiVehicle>(
-  //               builder: (context, provider, child) {
-  //                 return _buildMap(provider);
-  //               },
-  //             ),
-  //             Positioned(
-  //               bottom: 0,
-  //               left: 0,
-  //               child: Container(
-  //                 height: MediaQuery.of(context).size.height * 0.25,
-  //                 width: MediaQuery.of(context).size.width * 0.4,
-  //                 margin: const EdgeInsets.all(5),
-  //                 decoration: BoxDecoration(
-  //                   borderRadius: BorderRadius.circular(15.0),
-  //                   color: const Color(0x99808080)
-  //                 ),
-  //                 child: SingleChildScrollView(
-  //                   child: Column(
-  //                     mainAxisAlignment: MainAxisAlignment.center,
-  //                     children: [
-  //                       Row(
-  //                         mainAxisAlignment: MainAxisAlignment.center,
-  //                         children: [
-  //                           Radio(
-  //                             value: 'UDP',
-  //                             groupValue: _selectedProtocol.value,
-  //                             onChanged: (value) {
-  //                               setState(() {
-  //                                 _selectedProtocol.value = value as String;
-  //                               });
-  //                             },
-  //                           ),
-  //                           const Text('UDP'),
-  //                           Radio(
-  //                             value: 'TCP',
-  //                             groupValue: _selectedProtocol.value,
-  //                             onChanged: (value) {
-  //                               setState(() {
-  //                                 _selectedProtocol.value = value as String;
-  //                               });
-  //                             },
-  //                           ),
-  //                           const Text('TCP'),
-  //                         ],
-  //                       ),
-  //                       TextField(
-  //                         controller: _serverAddressController,
-  //                         decoration: const InputDecoration(labelText: 'Server Address'),
-  //                       ),
-  //                       TextField(
-  //                         controller: _serverPortController,
-  //                         keyboardType: TextInputType.number,
-  //                         decoration: const InputDecoration(labelText: 'Server Port'),
-  //                       ),
-  //                       const Padding(padding: EdgeInsets.only(bottom: 10)),
-  //                       ElevatedButton(
-  //                         onPressed: () async {
-  //                           final String serverAddress = _serverAddressController.text;
-  //                           final int serverPort = int.tryParse(_serverPortController.text) ?? 0;
-
-  //                           if (serverAddress.isNotEmpty && serverPort > 0) {
-  //                             if (_selectedProtocol.value == 'TCP') {
-  //                               await Provider.of<LinkTaskManager>(context, listen: false).startTCPTask(serverAddress, serverPort);
-  //                             } else {
-  //                               await Provider.of<LinkTaskManager>(context, listen: false).startUDPTask(serverAddress, serverPort);
-  //                             }
-  //                           }
-  //                         },
-  //                         child: const Text('포트 개방')
-  //                       )
-  //                     ],
-  //                   ),
-  //                 )
-  //               )
-  //             )
-  //           ],
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  //}
 }
